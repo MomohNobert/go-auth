80fb8240b9f33d2d818f5dcca0d930ea51feef31
Momoh Nobert <okhai0112@gmail.com>
2023-11-20 15:06:15 +0100

    added hashing with bcrypt

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MomohNobert/go-auth
 
 go 1.21.3
+
+require golang.org/x/crypto v0.15.0
--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+	pass := "123456789"
+
+	hashedPass, err := hashPassword(pass)
+	if err != nil {
+		panic(err)
+	}
+
+	err = comparePassword(pass, hashedPass)
+	if err != nil {
+		log.Fatal("not logged in")
+	}
+
+	log.Println("logged in!")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("error while generating bcrypt hash from password: %s", err)
+	}
+
+	return hash, nil
+}
+
+func comparePassword(password string, hashedPass []byte) error {
+	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+	if err != nil {
+		return fmt.Errorf("invalid password: %s", err)
+	}
+	return nil
 }
