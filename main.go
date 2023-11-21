25188af9d4c09d1ba0c198e988cd60e811f5f019
Momoh Nobert <okhai0112@gmail.com>
2023-11-21 13:24:17 +0100

    added hmac

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"fmt"
 	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+var key = []byte{}
+
 func main() {
+	for i := 1; i <= 64; i++ {
+		key = append(key, byte(i))
+	}
+
 	pass := "123456789"
 
 	hashedPass, err := hashPassword(pass)
@@ -38,4 +46,25 @@ func comparePassword(password string, hashedPass []byte) error {
 		return fmt.Errorf("invalid password test push: %s", err)
 	}
 	return nil
+}
+
+func signMessage(msg []byte) ([]byte, error) {
+	mac := hmac.New(sha512.New, key)
+	_, err := mac.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred during signing: %s", err)
+	}
+
+	sign := mac.Sum(nil)
+	return sign, nil
+}
+
+func checkSign(msg, sign []byte) (bool, error) {
+	newSign, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("error occurred during check signing: %s", err)
+	}
+
+	same := hmac.Equal(newSign, sign)
+	return same, nil
 }
